f0d3f02e66da0dc98ef6c3b431a63b9554ef2e42
Diyor Khasanov <74970159+khasanovdiyor@users.noreply.github.com>
2021-03-05 11:58:44 +0500

    create infoBox component

---
+++ components/InfoBox.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="flex justify-between px-16">
+    <div class="flex items-center justify-between w-1/6">
+      <img src="../assets/images/truck.svg" alt="truck icon" class="w-16" />
+      <div>
+        <h3>Title</h3>
+        <span>Paragraph text</span>
+      </div>
+    </div>
+    <div class="flex items-center justify-between w-1/6">
+      <img
+        src="../assets/images/star-badge.svg"
+        alt="truck icon"
+        class="w-16"
+      />
+      <div>
+        <h3>Title</h3>
+        <span>Paragraph text</span>
+      </div>
+    </div>
+    <div class="flex items-center justify-between w-1/6">
+      <img src="../assets/images/headset.svg" alt="truck icon" class="w-16" />
+      <div>
+        <h3>Title</h3>
+        <span>Paragraph text</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
