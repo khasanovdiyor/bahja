77a0d2e508364483fa1f9ebc84960f3069c4cbf4
SardorTurdiboyev <sardorturdiboyev717@gmail.com>
2021-03-09 15:22:32 +0500

    add TheFooter component

---
+++ components/TheFooter.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <div class=" footer px-5 lg:px-16 pt-10 block md:flex justify-between">
+      <div
+        class=" row flex w-2/5 justify-between
+      "
+      >
+        <div class="col-5 md:col-3 categories leading-loose">
+          <h3 class="font-semibold mb-3 text-gray-700">KATEGORIYA</h3>
+          <ul class="text-gray-600">
+            <li><a href="#" class="">Ayollar </a></li>
+            <li><a href="#" class="">Erkaklar</a></li>
+            <li><a href="#" class="">Aksessuarlar</a></li>
+          </ul>
+        </div>
+        <div class="col-5 md:col-3 information leading-loose">
+          <h3 class="font-semibold mb-3 text-gray-700">MA'LUMOTLAR</h3>
+          <ul class="text-gray-600">
+            <li><a href="#" class="">Biz haqimizda</a></li>
+            <li><a href="#" class="">Bog'lanish</a></li>
+          </ul>
+        </div>
+      </div>
+
+      <div
+        class=" row flex w-2/5 justify-between
+      "
+      >
+        <div class="col-5 md:col-3 links leading-loose">
+          <h3 class="font-semibold mb-3 text-gray-700">FOYDALI LINKLAR</h3>
+          <ul class="text-gray-600">
+            <li><a href="#" class="">Xarid qilish </a></li>
+            <li><a href="#" class="">Yetkazib berish</a></li>
+            <li>
+              <a href="#" class="">Qaytarish va almashtirish</a>
+            </li>
+          </ul>
+        </div>
+        <div class="col-5 md:col-3 contacts leading-loose">
+          <h3 class="font-semibold mb-3 text-gray-700">BOG'LANISH</h3>
+          <ul class="text-gray-600">
+            <li>
+              <a href="#" class="flex">
+                <img
+                  src="../assets/images/placeholder.svg"
+                  alt=""
+                  class="w-5 mr-3"
+                />
+                Biz haqimizda</a
+              >
+            </li>
+            <li>
+              <p class="flex">
+                <img src="../assets/images/phone.svg" alt="" class="w-5 mr-3" />
+                +998 93 627 58 98
+              </p>
+            </li>
+            <li>
+              <a href="#" class="flex">
+                <img
+                  src="../assets/images/envelope.svg"
+                  alt=""
+                  class="w-5 mr-3"
+                />bahja@gmail.com</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="bg-gray-200 h-10">
+      <!-- <hr class="" /> -->
+      <span
+        class="w-full text-center block mx-auto mt-5 py-2 text-sm text-gray-500"
+        >Bahja.uz Copyright &copy; 2021
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
