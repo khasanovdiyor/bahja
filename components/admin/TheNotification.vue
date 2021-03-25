1779098fbc4b047a9f5d4416a3fb443706efa92b
Diyor Khasanov <74970159+khasanovdiyor@users.noreply.github.com>
2021-03-25 09:50:35 +0500

    create Notification component

---
+++ components/admin/TheNotification.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="fixed top-0 py-4 px-6 flex justify-between">
+    <slot>This is default content</slot>
+    <!-- <div class="">X</div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheNotification"
+};
+</script>
+
+<style></style>
