7e72a5887d3a3e96a89041b502ecffbee1676d64
Diyor Khasanov <74970159+khasanovdiyor@users.noreply.github.com>
2021-03-31 14:19:01 +0500

    make global.js mixin global

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,5 +3,10 @@
     <Nuxt />
   </div>
 </template>
-
+<script>
+import Vue from "vue";
+import global from "~/mixins.js/global.js";
+Vue.mixin(global);
+export default {};
+</script>
 <style></style>
--- mixins.js/global.js
+++ mixins.js/global.js
@@ -1,7 +1,21 @@
 export default {
   data() {
     return {
-      selectedProducts: []
+      selectedProducts: [],
+      categories: []
     };
+  },
+  methods: {
+    getCategories() {
+      this.$axios
+        .get("product/category-all")
+        .then(res => {
+          this.categories = res.data;
+          console.log("global categories", this.categories);
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   }
 };
