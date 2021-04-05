f1b29f1cce15716b9255069468f7ed94a0082cd0
Diyor Khasanov <74970159+khasanovdiyor@users.noreply.github.com>
2021-04-05 12:21:22 +0500

    add products api

---
+++ api/product.js
@@ -0,0 +1,108 @@
+export default {
+  show: {
+    name: "Iphone 12 Pro MAX",
+    price: "13324",
+    image:
+      "https://assets.swappie.com/swappie-product-iphone-12-pro-max-silver.png",
+    images: [
+      "https://media.wired.com/photos/5fa5e735ba670daaf8e97a91/master/w_2560%2Cc_limit/GEAR-MAX-Apple-iPhone-12-Pro-Max-SOURCE-Apple.jpg"
+    ],
+    attributes: {
+      color: {
+        type: "color", // color, text, image
+        label: "Rang",
+        value: "#ffffff"
+      },
+      size: {
+        type: "text",
+        label: "O'lcham",
+        value: 32
+      },
+      type: {
+        type: "text",
+        label: "Turi",
+        value: "koja"
+      }
+    },
+    variations: [
+      {
+        name: "Iphone 12 Pro MAX",
+        price: "13324",
+        image:
+          "https://img.router-switch.com/media/customoptions/129/2/4/iphone-12-pro-max-blue.jpg",
+        images: [
+          "https://assets.swappie.com/swappie-product-iphone-12-pro-max-pacific-blue-back.png"
+        ],
+        attributes: {
+          color: {
+            type: "color", // color, text, image
+            label: "Rang",
+            value: "#000000"
+          },
+          size: {
+            type: "text",
+            label: "O'lcham",
+            value: 64
+          },
+          type: {
+            type: "text",
+            label: "Turi",
+            value: "koja"
+          }
+        }
+      },
+      {
+        name: "Iphone 12 Pro MAX",
+        price: "13324",
+        image:
+          "https://img.router-switch.com/media/customoptions/129/2/4/iphone-12-pro-max-blue.jpg",
+        images: [
+          "https://assets.swappie.com/swappie-product-iphone-12-pro-max-pacific-blue-back.png"
+        ],
+        attributes: {
+          color: {
+            type: "color", // color, text, image
+            label: "Rang",
+            value: "#000000"
+          },
+          size: {
+            type: "text",
+            label: "O'lcham",
+            value: 128
+          },
+          type: {
+            type: "text",
+            label: "Turi",
+            value: "koja"
+          }
+        }
+      },
+      {
+        name: "Iphone 12 Pro MAX",
+        price: "13324",
+        image:
+          "https://assets.swappie.com/swappie-product-iphone-12-pro-max-silver.png",
+        images: [
+          "https://media.wired.com/photos/5fa5e735ba670daaf8e97a91/master/w_2560%2Cc_limit/GEAR-MAX-Apple-iPhone-12-Pro-Max-SOURCE-Apple.jpg"
+        ],
+        attributes: {
+          color: {
+            type: "color", // color, text, image
+            label: "Rang",
+            value: "#ffffff"
+          },
+          size: {
+            type: "text",
+            label: "O'lcham",
+            value: 64
+          },
+          type: {
+            type: "text",
+            label: "Turi",
+            value: "koja"
+          }
+        }
+      }
+    ]
+  }
+};
