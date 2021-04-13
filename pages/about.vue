ef6de4aa717f772db8004060795b34e4bd6e38f2
SardorTurdiboyev <sardorturdiboyev717@gmail.com>
2021-04-13 18:49:09 +0500

    add about page

---
+++ pages/about.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <TheContact />
+    <TheHeader />
+    <div class="px-16">
+      <p class="text-center text-xl font-bold text-gray-700  my-10">
+        Biz haqimizda
+      </p>
+      <div>
+        <p class="flex justify-center tracking-wide">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+          incidunt sit quas ipsum rem, consequatur veniam necessitatibus illo
+          deserunt distinctio illum quibusdam voluptatem adipisci, obcaecati
+          optio iste asperiores nihil. Dignissimos aperiam neque saepe iure
+          dolor, illum sequi delectus adipisci assumenda, molestias dicta!
+          <br />Quam nostrum nesciunt reiciendis labore accusantium, rerum
+          fugiat! Ab, eius! Fuga id, laborum ad magnam nobis itaque! Pariatur
+          quis rerum perferendis alias, earum ab minima. Totam, maiores ducimus
+          eum, fugiat deserunt distinctio ullam corporis est nisi ipsum dolorum
+          reprehenderit eos saepe sapiente quae tempore in eveniet blanditiis
+          accusamus non beatae quos et quasi quas?<br />
+          Quos, molestiae aperiam accusantium, et voluptate repellendus facilis
+          dolorem enim saepe assumenda animi porro non nostrum voluptatibus
+          consequatur aut quam temporibus nobis! Tempore numquam, ratione
+          perspiciatis ullam quibusdam dolores in id fugit, assumenda quam
+          obcaecati dolore odio minus. Asperiores laudantium obcaecati
+          architecto ex unde voluptatibus minus aut temporibus consequuntur hic
+          quis commodi est, repellat magni accusamus dolorum, corrupti
+          provident, earum voluptatum cum. Reiciendis sint id, aliquam magni,
+          aliquid iste cumque amet omnis, laboriosam dolore ducimus iusto rem
+          aut officiis repellat mollitia quibusdam.<br />
+          Doloremque deserunt veniam eos fugiat, sapiente recusandae nam
+          laudantium officiis dicta! Culpa iste libero mollitia? Placeat
+          molestias perspiciatis asperiores repellendus doloremque at!
+        </p>
+      </div>
+    </div>
+    <TheFooter />
+  </div>
+</template>
