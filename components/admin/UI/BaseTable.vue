a24754fbf03800cab0b350a8fc092978d1d770d9
Diyor Khasanov <74970159+khasanovdiyor@users.noreply.github.com>
2021-05-03 16:58:53 +0500

    add BaseTabel component

---
+++ components/admin/UI/BaseTable.vue
@@ -0,0 +1,34 @@
+<template>
+  <table class="min-w-full divide-y divide-gray-200">
+    <thead class="bg-gray-200">
+      <tr>
+        <th
+          scope="col"
+          class="px-6 py-3 w-16 text-left text-xs font-bold text-gray-600 uppercase"
+          v-for="(head, index) in headers"
+          :key="index"
+        >
+          {{ head }}
+        </th>
+      </tr>
+    </thead>
+    <tbody class="bg-white">
+      <slot name="body"></slot>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "BaseTable",
+  props: {
+    headers: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+</style>
