25ce118e9d155bd440b46674c64f4a40b9c782b0
Diyor Khasanov <74970159+khasanovdiyor@users.noreply.github.com>
2021-05-03 17:01:29 +0500

    add BaseButtonLink component

---
+++ components/admin/UI/BaseButtonLink.vue
@@ -0,0 +1,33 @@
+<template>
+  <div
+    class="bg-gray-800 rounded-md text-sm text-white capitalize inline-block"
+  >
+    <nuxt-link v-if="link" :to="url" class="py-2 px-4 block">{{
+      buttonText
+    }}</nuxt-link>
+    <button v-else @click="$emit('button-click')" class="py-2 px-4">
+      {{ buttonText }}
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: "BaseButtonLink",
+  props: {
+    link: {
+      type: Boolean,
+      default: false,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    url: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style></style>
