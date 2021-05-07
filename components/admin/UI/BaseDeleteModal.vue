ef6cb14ccdb2cd1770d0e49a330274ed971049ce
Diyor Khasanov <74970159+khasanovdiyor@users.noreply.github.com>
2021-05-07 18:32:34 +0500

    add alert prop

--- components/admin/UI/BaseDeleteModal.vue
+++ components/admin/UI/BaseDeleteModal.vue
@@ -4,6 +4,9 @@
   >
     <div class="w-1/3 rounded-md mx-auto bg-white py-4 px-10">
       <span class="font-bold text-center text-xl block mb-6">{{ text }}</span>
+      <span class="text-center text-xl block mb-6 text-red-600">{{
+        alert
+      }}</span>
       <div class="flex justify-between">
         <button
           @click="$emit('delete')"
@@ -28,6 +31,10 @@ export default {
     text: {
       type: String,
       required: true
+    },
+    alert: {
+      type: String,
+      default: ""
     }
   }
 };
