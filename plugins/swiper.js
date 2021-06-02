a9197b15c7561005a91e1b5d7a239c722e34317f
Diyor Khasanov <74970159+khasanovdiyor@users.noreply.github.com>
2021-06-02 11:05:26 +0500

    change swiper style

--- plugins/swiper.js
+++ plugins/swiper.js
@@ -1,6 +1,6 @@
 import Vue from "vue";
 import VueAwesomeSwiper from "vue-awesome-swiper";
 
-// import style (>= Swiper 6.x)
-import "swiper/css/swiper.css";
+// // import style (>= Swiper 6.x)
+import "@/assets/css/swiper.css";
 Vue.use(VueAwesomeSwiper);
