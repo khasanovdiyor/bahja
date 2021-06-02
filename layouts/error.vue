df9a2f2c77c0d797b6c97173e5be70e821e35f6b
Diyor Khasanov <74970159+khasanovdiyor@users.noreply.github.com>
2021-06-02 13:02:27 +0500

    change bg color of link

--- layouts/error.vue
+++ layouts/error.vue
@@ -16,7 +16,7 @@
 
         <nuxt-link
           to="/"
-          class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-blue-600 active:bg-blue-600 hover:bg-blue-700"
+          class="px-4 inline py-2 text-sm font-medium leading-5 shadow text-white transition-colors duration-150 border border-transparent rounded-lg focus:outline-none focus:shadow-outline-blue bg-gray-800 active:bg-blue-600 hover:bg-blue-700"
         >
           Asosiy sahifaga o'tish
         </nuxt-link>
